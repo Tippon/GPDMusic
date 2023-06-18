3b8c3200417cc94261f6922073e4d53b194cf837
Tippon <42301328+Tippon@users.noreply.github.com>
2023-06-18 03:25:58 +0100

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,21 @@
+const footerContent = `<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
+	<a href="https://www.facebook.com/CwmamanMusicFestival/" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a>
+	<a href="https://www.instagram.com/cwmaman_music_festival/" target="_blank"><i class="fa-brands fa-instagram fa-2x"></i></a>
+	<a href="https://twitter.com/CwmFest" target="_blank"><i class="fa-brands fa-twitter fa-2x"></i></a>
+	<a href="https://www.flickr.com/photos/127740634@N02/" target="_blank"><i class="fa-brands fa-flickr fa-2x"></i></a>
+	<a href="https://www.youtube.com/user/CwmFest" target="_blank"><i class="fa-brands fa-youtube fa-2x"></i></a>
+</footer>
+<br>
+<credits class="w3-container w3-center w3-content">
+	<h5 class="w3-wide">Website and app design by Ryan at GPD Music</h5>
+</credits>`;
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = footerContent;
+  }
+}
+
+customElements.define("footer-component", Footer);
---
+++ js/header.js
@@ -0,0 +1,25 @@
+const headerContent = `			<div class="banner">
+				<a href="index.html"><img src="images/Cover.png" alt="GPD Music"></a>
+			</div>
+		<br>
+		<nav>
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="about.html">About GPD Music</a></li>
+			</ul>
+		</nav>
+		<br>
+		<nav>
+			<ul>
+				<li><a href="bandfinder.html">BandFinder</a></li>
+			</ul>
+		</nav>`;
+
+class Header extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = headerContent;
+  }
+}
+
+customElements.define("header-component", Header);
